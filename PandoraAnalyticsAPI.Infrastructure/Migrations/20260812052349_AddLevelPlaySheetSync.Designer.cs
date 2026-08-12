bf931cc1d80bfb3c47a7d64a13660905267a75bd
Raeisi <36035391+BabakRaeisi@users.noreply.github.com>
2026-08-12 01:26:25 -0400

    Add Google Sheets sync tracking for level plays

--- PandoraAnalyticsAPI.Infrastructure/Migrations/20260812052349_AddLevelPlaySheetSync.Designer.cs
+++ PandoraAnalyticsAPI.Infrastructure/Migrations/20260812052349_AddLevelPlaySheetSync.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PandoraAnalyticsAPI.Infrastructure.Data;
@@ -11,9 +12,11 @@ using PandoraAnalyticsAPI.Infrastructure.Data;
 namespace PandoraAnalyticsAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812052349_AddLevelPlaySheetSync")]
+    partial class AddLevelPlaySheetSync
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PandoraAnalyticsAPI.Infrastructure/Migrations/20260812052349_AddLevelPlaySheetSync.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PandoraAnalyticsAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLevelPlaySheetSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SheetSynced",
+                table: "LevelPlays",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SheetSynced",
+                table: "LevelPlays");
+        }
+    }
+}
