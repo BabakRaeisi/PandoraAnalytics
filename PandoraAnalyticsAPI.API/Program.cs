3745160a6888b20609de70f72f1aebb35e1fb817
Raeisi <36035391+BabakRaeisi@users.noreply.github.com>
2026-08-12 00:28:50 -0400

    Apply database migrations on startup

--- PandoraAnalyticsAPI.API/Program.cs
+++ PandoraAnalyticsAPI.API/Program.cs
@@ -31,7 +31,11 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 });
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
